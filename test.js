db70e4d2bccbf51c931455b564d8e6d5d4a0a319
Ronnie <ronnie94official@gmail.com>
2024-07-03 21:05:44 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,49 @@
+const search = (arr, x, start, end) => {
+  if (start > end) return false;
+  let mid = Math.floor((start + end) / 2);
+  if (arr[mid] === x) return true;
+  if (arr[mid] > x) {
+    return search(arr, x, start, mid - 1);
+  } else {
+    return search(arr, x, mid + 1, end);
+  }
+};
+
+
+const getApplicationID = () => {
+  var appID = "";
+  gs.log("appid: " + this.getParameter("sysparm_appName"), "pipeline");
+  var grAppID = new GlideRecord("cmdb_ci_business_app");
+  if (grAppID.get(this.getParameter("sysparm_appname"))) {
+    appID = grAppID.number.toString();
+    gs.log("appid: " + appID, "pipeline");
+  }
+ return appID;
+}
+
+function newGeneration(cells) {
+  const nextGeneration = []
+  for (let i = 0; i < cells.length; i++) {
+    const nextGenerationRow = []
+    for (let j = 0; j < cells[i].length; j++) {
+      let neighbourCount = 0
+      if (i > 0 && j > 0) neighbourCount += cells[i - 1][j - 1]
+      if (i > 0) neighbourCount += cells[i - 1][j]
+      if (i > 0 && j < cells[i].length - 1)
+        neighbourCount += cells[i - 1][j + 1]
+      if (j > 0) neighbourCount += cells[i][j - 1]
+      if (j < cells[i].length - 1) neighbourCount += cells[i][j + 1]
+      if (i < cells.length - 1 && j > 0) neighbourCount += cells[i + 1][j - 1]
+      if (i < cells.length - 1) neighbourCount += cells[i + 1][j]
+      if (i < cells.length - 1 && j < cells[i].length - 1)
+        neighbourCount += cells[i + 1][j + 1]
+      const alive = cells[i][j] === 1
+      const cellIsAlive =
+        (alive && neighbourCount >= 2 && neighbourCount <= 3) ||
+        (!alive && neighbourCount === 3)
+      nextGenerationRow.push(cellIsAlive ? 1 : 0)
+    }
+    nextGeneration.push(nextGenerationRow)
+  }
+  return nextGeneration
+}
